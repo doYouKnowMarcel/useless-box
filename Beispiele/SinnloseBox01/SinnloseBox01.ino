8912b189a39b0c13500e089d73a4df83f343d125
Marcel <96464741+doYouKnowMarcel@users.noreply.github.com>
2022-06-11 12:33:06 +0200

    Add files via upload

--- Beispiele/SinnloseBox01/SinnloseBox01.ino
+++ Beispiele/SinnloseBox01/SinnloseBox01.ino
@@ -5,38 +5,42 @@
 Servo servo_driver;  // create servo object to control a servo
 
 int servo_pos = 0;    // variable to store the servo position
-const int servo_min = 55;    //  Servo Min-Stellung
-const int servo_max = 150;    // Servo Max-Stellung
+const int servo_closed = 95;    //  Servo Min-Stellung
+int servo_opened = 10;    // Servo Max-Stellung
 int servo_range = 0;  
 
 const int button_pin = 3;  // Eingang Kippstellung
 int button_state = 0; 
 bool button_debug = false; 
 
+int buzzerPin = 6;
+
 void setup() {
   servo_driver.attach(2);  // attach servo on pin 2
-  pinMode(button_pin, INPUT); //attach button on pin 3
-  music.init(4); // attach music buzzer on pin 4
+  pinMode(button_pin, INPUT_PULLUP); //attach button on pin 3
+  music.initBuzzer(6); // attach music buzzer on pin 4
   Serial.begin(9600);
 
   servoMaxInit();
-
 }
 
 void loop() {
   //Button position einlesen
   button_state = digitalRead(button_pin);
   delay(100);
-  if(button_state == HIGH || button_debug) {
+  if(buttonPushed() || button_debug) {
     Serial.println("button on");
     
     button_debug = false;
 
     delay(300);
-    long programm = random(0,2); //0 oder 1
+    long programm = random(0,3); //0 oder 1
     Serial.print("Ausführen von Programm ");
     Serial.println(programm);
     switch (programm) {
+      case 2:
+        servoButtonOffVariante2();
+        break;
       case 1:
         servoButtonOffVariante1();
         break;
@@ -50,19 +54,16 @@ void loop() {
   }
 }
 
+
+bool buttonPushed(){
+  button_state = digitalRead(button_pin);
+  return (button_state == LOW);
+}
+
 void servoButtonOffVariante0(){
-  music.starwars(12);
-  
+  music.dumdadadumdumdumdum(12);
+
   /*
-    Author: Everton Ramires
-    More Tutorial: https://www.youtube.com/channel/UC7zG4YQJc8v-9jNGQknOx5Q
-  
-    Based on the great work of robsoncouto at github (https://github.com/robsoncouto/arduino-songs).
-  
-    CURRENT SONG OPTIONS:
-    (PT-BR)OPÇÕES ATUAIS DE MUSICAS:
-  
-  
     music.asabranca();
     music.babyelephantwalk();
     music.bloodytears();
@@ -103,62 +104,109 @@ void servoButtonOffVariante0(){
     music.zeldatheme();
   */
 
-    servoMove(servo_max, 1.0);
+    servoMove(servo_opened, 1.0);
     delay(1000);
-    servoMove(servo_min, 0.3);
-    delay(2000);
+    servoMove(servo_closed, 0.3);
+    delay(1000);
 }
 void servoButtonOffVariante1(){
     //vor mit kleiner Geschwindigkeit bis zur Hälfte
-    servoMove(servo_min + (servo_range)/2, 0.1);
+    servoMovePerc(0.6, 0.1);
     delay(1000);
     
     //bis zum Ende Fahren mit hoher Geschwindigkeit
-    servoMove(servo_max, 1.0);
+    servoMove(servo_opened, 1.0);
+    delay(200);
+    
+    servoMovePerc(0.55, 0.1);
+    delay(500);
+    
+    servoMovePerc(0.8, 1.0);
+
+    //zurück
+    servoMove(servo_closed, 0.7);
+}
+
+void servoButtonOffVariante2(){
+    //vor mit kleiner Geschwindigkeit bis zur Hälfte
+    servoMovePerc(0.6, 0.1);
+    delay(1000);
+    
+    //bis zum Ende Fahren mit hoher Geschwindigkeit
+    servoMove(servo_opened, 1.0);
     delay(200);
 
     //zurück
-    servoMove(servo_min, 1);
+    servoMove(servo_closed, 0.7);
 }
 
-void servoMaxInit(){
-  /*
-  for (servo_pos = servo_pos; servo_pos <= 180; servo_pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo_driver.write(servo_pos);              // tell servo to go to position in variable 'pos'
-    delay(20);                       // waits 15 ms for the servo to reach the position
-    
+void servoMovePerc(float servo_pos_perc, float pace){
+  float servo_open_val = 0;
+  if(servo_closed > servo_opened){
+    servo_open_val = servo_range*servo_pos_perc;
+  }else{
+    servo_open_val = servo_range*(1-servo_pos_perc);
   }
-  */
-  
-  
-  servo_range = servo_max - servo_min;
-  servoMove(servo_min, 1);
+  servoMove(servo_closed + servo_open_val, pace);
 }
 
 //moves servo to servo_pos_end with pace (0.1=slow=10%, 1=fast=100%)
-void servoMove(int servo_pos_end, int pace){
+void servoMove(int servo_pos_end, float pace){
   int servo_delay = (int) (20-20*pace+2);
+  
+  Serial.print("Servo von ");
+  Serial.print(servo_pos);
+  Serial.print(" nach ");
+  Serial.println(servo_pos_end);
+
+  bool buttonPush = buttonPushed();
   
   //init servo step
   if(servo_pos<=servo_pos_end){
-    //for (servo_pos = servo_min; servo_pos <= servo_max; servo_pos += 1) { // goes from 0 degrees to 180 degrees
+    //for (servo_pos = servo_closed; servo_pos <= servo_opened; servo_pos += 1) { // goes from 0 degrees to 180 degrees
     for (servo_pos = servo_pos; servo_pos <= servo_pos_end; servo_pos += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       servo_driver.write(servo_pos);              // tell servo to go to position in variable 'pos'
       delay(servo_delay);                       // waits 15 ms for the servo to reach the position
 
       //check if button end reached
-      /*
-      button_state = digitalRead(button_pin);
-      if(button_state == LOW)return;
-      */
+      if(!buttonPush){
+        if(buttonPushed())return;
+      }
     }
   }else{
-    for (servo_pos = servo_max; servo_pos >= servo_pos_end; servo_pos -= 1) { // goes from 180 degrees to 0 degrees
+    for (servo_pos = servo_opened; servo_pos >= servo_pos_end; servo_pos -= 1) { // goes from 180 degrees to 0 degrees
       servo_driver.write(servo_pos);              // tell servo to go to position in variable 'pos'
       delay(servo_delay);                       // waits 15 ms for the servo to reach the position
+      
+      if(!buttonPush){
+        if(buttonPushed())return;
+      }
     }
   }
   //Serial.println(servo_pos);
+}
+
+void servoMaxInit(){
+  /*
+  // set bounds of servo automatically
+  if(!buttonPushed() && servo_opened == -1) {
+    int pos = 0;
+    for (servo_pos = servo_closed; servo_pos >= 0; servo_pos -= 1) { // goes from 0 degrees to 180 degrees
+      servo_driver.write(servo_pos); 
+      if(buttonPushed()){
+        servo_opened = servo_pos;
+        break;
+      }
+      delay(5);                       // waits 15 ms for the servo to reach the position
+    }
+  }
+  */
+  
+  Serial.print("Servo range: ");
+  servo_range = (servo_opened - servo_closed);
+  Serial.println(servo_range);
+  
+  Serial.println("Close servo");
+  servoMove(servo_closed, 1);
 }
